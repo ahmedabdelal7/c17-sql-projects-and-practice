6b4bdacba37a6c663ec81611d85800acea672edc
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-08 20:58:03 +0300

    Add (Problem-39)

--- SQL Problems/SQL Problems.sql
+++ SQL Problems/SQL Problems.sql
@@ -499,7 +499,10 @@ select count(*) NumberOfVehiclesAboveAverageEngineCC  from
 select distinct Engine_CC from VehicleDetails
 order by Engine_CC DESC
 
+--Problem 39: Get the maximum 3 Engine CC
 
+select distinct top 3 Engine_CC from VehicleDetails
+order by Engine_CC DESC
 
 
 
