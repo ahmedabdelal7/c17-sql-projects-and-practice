bdb804ff09443ceb9827d2a3198633b703fc59dc
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-08 19:43:16 +0300

    Add (Problem-28)

--- SQL Problems/SQL Problems.sql
+++ SQL Problems/SQL Problems.sql
@@ -397,6 +397,13 @@ select Bodies.BodyName ,VehicleDetails.*
 from VehicleDetails inner join Bodies on VehicleDetails.BodyID = Bodies.BodyID
 where Bodies.BodyName IN ('Coupe','Hatchback','Sedan')
 
+--Problem 28: Get all vehicles that their body is 'Coupe' or 'Hatchback' or 'Sedan' 
+--and manufactured in year 2008 or 2020 or 2021
+
+select Bodies.BodyName ,VehicleDetails.* 
+from VehicleDetails inner join Bodies on VehicleDetails.BodyID = Bodies.BodyID
+where Bodies.BodyName IN ('Coupe','Hatchback','Sedan') 
+	AND Year IN (2008,2020,2021)
 
 
 
