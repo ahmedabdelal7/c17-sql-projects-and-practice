cc3d8c8ee5bc49c9171c5ea3c958fb120b25889f
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-08 03:06:50 +0300

    Add (Problem-7)

--- SQL Problems/SQL Problems.sql
+++ SQL Problems/SQL Problems.sql
@@ -219,9 +219,20 @@ group by m.Make
 order by NumOfVehicles DESC
 
 
+-- Problem 7: Get number of vehicles made between 1950 and 2000 per make and add total vehicles column beside it, then calculate it's percentage
 
+select *, CAST(R1.NumOfVehicles as float )/ CAST( R1.TotalVehicles as float ) as Perc from 
+(
 
+		select 
+			m.Make , count(*) as NumOfVehicles, TotalVehicles = (select count(*) from VehicleDetails)
+		from Makes m 
+		inner join VehicleDetails vd on m.MakeID = vd.MakeID
+		where year between 1950 and 2000   
+		group by m.Make
 
+)R1
+order by NumOfVehicles DESC
 
 
 
