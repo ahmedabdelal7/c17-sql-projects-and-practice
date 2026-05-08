353d3ba4625876f919a43cd46d202160c6b28d54
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-05-08 20:27:07 +0300

    Add (Problem-31)

--- SQL Problems/SQL Problems.sql
+++ SQL Problems/SQL Problems.sql
@@ -444,10 +444,12 @@ select distinct NumDoors
 from VehicleDetails
 order by NumDoors ;
 
+--Problem 31: Get all Vehicle_Display_Name, year and add extra column to calculate the age of the car 
+--then sort the results by age desc.
 
+select Vehicle_Display_Name, Year, Age = Year(getDate()) - Year
 
-
-
+from VehicleDetails
 
 
 
